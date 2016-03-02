27b61a513c80fe7846ef85c26ff8d942fe5430a7
BanMasanobu <babababandits@gmail.com>
2016-03-03 07:34:04 +0900

    add controller

---
+++ pocker/app/controllers/pocker_controller.rb
@@ -0,0 +1,7 @@
+class PockerController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- pocker/config/routes.rb
+++ pocker/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pocker/index'
+
+  get 'pocker/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
